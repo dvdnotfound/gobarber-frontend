4cc5daf9578c77eb334d6ea37a9f96447a84cdec
David Santos <contato.dvdsantos@gmail.com>
2020-11-18 03:02:47 -0300

    login

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import SignIn from './pages/SignIn/index';
 // import SignUp from './pages/SignUp/index';
 import GlobalStyle from './styles/global';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <AuthContext.Provider value={{ name: 'Diego' }}>
+    <AuthProvider>
       <SignIn />
       <GlobalStyle />
-    </AuthContext.Provider>
+    </AuthProvider>
   </>
 );
 
